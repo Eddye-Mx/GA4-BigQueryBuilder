9c1409eed32afe0ed695e40ce7c8c4b6aae9baa6
Eddye <eddyemx@gmail.com>
2023-04-26 17:03:06 -0600

    added ifnull function to query builder

--- main.go
+++ main.go
@@ -6,17 +6,19 @@ import (
 )
 
 var entriesToOutputs = map[string]string{
-	"campaign": "traffic_source.name as campaign", /*Dimension*/
-	"source": "traffic_source.source as source",/*Dimension*/
-	"medium": "traffic_source.medium as medium",/*Dimension*/
-	"date": "event_date as date",/*Dimension*/
-	"sessions": "COUNT(case event_name when 'session_start' then 1 else null end) as sessions",/*Metric*/
-	"pageviews": "COUNT(case event_name when 'page_view' then 1 else null end) as pageviews",/*Metric*/
-	"users": "COUNT(DISTINCT user_pseudo_id) as users",/*Metric*/
-	"new_users": "COUNT(case event_name when 'first_visit' then 1 else null end) as new_users",/*Metric*/
-	"revenue": "SUM(ecommerce.purchase_revenue) as revenue",/*Metric*/
-	"transactions": "COUNT(case event_name when 'purchase' then 1 else null end) as transactions",/*Metric*/
-	"item_revenue": "SUM(i.item_revenue) as item_revenue",/*Metric*/
+	"Campaign": "IFNULL(traffic_source.source,'(not set)') as Campaign", /*Dimension (IS USER CAMPAIGN?)*/ 
+	"Source": "IFNULL(traffic_source.source,'(not set)') as Source",/*Dimension (IS USER SOURCE?)*/
+	"Medium": "IFNULL(traffic_source.medium,'not set)') as Medium",/*Dimension (IS USER MEDIUM?)*/
+	"Date": "event_date as Date",/*Dimension*/
+	"Sessions": "IFNULL(COUNT(case event_name when 'session_start' then 1 else null end),0) as Sessions",/*Metric*/
+	"Pageviews": "IFNULL(COUNT(case event_name when 'page_view' then 1 else null end),0) as Pageviews",/*Metric*/
+	"Users": "IFNULL(COUNT(DISTINCT user_pseudo_id),0) as Users",/*Metric*/
+	"New_users": "IFNULL(COUNT(case event_name when 'first_visit' then 1 else null end),0) as New_users",/*Metric*/
+	"Revenue": "IFNULL(SUM(ecommerce.purchase_revenue),0) as Revenue",/*Metric*/
+	"Transactions": "IFNULL(COUNT(case event_name when 'purchase' then 1 else null end),0) as Transactions",/*Metric*/
+	"Revenue_usd": "IFNULL(SUM(ecommerce.purchase_revenue_in_usd),0) as Revenue_usd",
+	"Average_LTV": "AVG(user_ltv.revenue) as Average_LTV ",
+	"item_revenue": "IFNULL(SUM(i.item_revenue),0) as item_revenue",/*Metric Not Working****/
 }
 
 var clientToDatasets = map[string]accountInfo{
@@ -34,58 +36,63 @@ var payload = Payload{
 	   },
 	},
 	Dimensions:[]string{
-		"date",
-	   "source",
-	   "medium",
-	   "campaign",
+		"Date",
+	   "Source",
+	   "Medium",
+	   "Campaign",
 	},
 	Metrics:[]string{
-	   "users",
-	   "new_users",
-	   "pageviews",
-	   "transactions",
-	   "revenue",
+	   "Users",
+	   "New_users",
+	   "Pageviews",
+	   "Transactions",
+	   "Revenue",
+	   "Average_LTV",
 	},
 	Start_date:"2023-04-10",
 	End_date:"2023-04-12",
 }
 
 func main() {
-	fmt.Println(concatSQL(payload))
+	theString, err := concatSQL(payload)
+	if err != nil {
+		fmt.Println("error: ",err)
+	}
+	fmt.Println(theString)
 
 }
 
 
 func concatSQL(payload Payload) (string, error){
-	toReturn := "SELECT "
+	toReturn := "SELECT \n"
 	toReturn += entriesToOutputs[payload.Dimensions[0]] 
 	if(len(payload.Dimensions)>0){
-		toReturn += ","
+		toReturn += ",\n"
 		for i:= 1; i<len(payload.Dimensions); i++ {
 			toReturn += entriesToOutputs[payload.Dimensions[i]] 
-			toReturn += ","
+			toReturn += ",\n"
 		}
 	}
 	toReturn += entriesToOutputs[payload.Metrics[0]] 
 	if(len(payload.Metrics)>0){
 		for i:= 1; i<len(payload.Metrics); i++ {
-			toReturn += ","
+			toReturn += ",\n"
 			toReturn += entriesToOutputs[payload.Metrics[i]] 
 		}
 	}
-	groupBy := "group by " + payload.Dimensions[0]
+	groupBy := "\ngroup by " + payload.Dimensions[0]
 	if(len(payload.Dimensions)>0){
 		for i:= 1; i<len(payload.Dimensions); i++ {
 			groupBy += ","
 			groupBy += payload.Dimensions[i]
 		}
 	}
-	orderBy := fmt.Sprintf("order by %v",(len(payload.Dimensions)+1))
+	orderBy := fmt.Sprintf("\n order by %v",(len(payload.Dimensions)+1))
 	fmt.Println(groupBy)
 	sDateParsed := strings.ReplaceAll(payload.Start_date, "-", "")
 	eDateParsed := strings.ReplaceAll(payload.End_date, "-", "")
 	ProjectInfo := clientToDatasets[payload.Accounts[0].Name]
-	toReturn = fmt.Sprintf(toReturn + " FROM `%s.%s.events_*` where _TABLE_SUFFIX BETWEEN '%s' AND '%s' %s %s desc",ProjectInfo.Project,ProjectInfo.Dataset, sDateParsed, eDateParsed, groupBy, orderBy)
+	toReturn = fmt.Sprintf(toReturn + " FROM `%s.%s.events_*` \n where _TABLE_SUFFIX BETWEEN '%s' AND '%s' %s %s desc",ProjectInfo.Project,ProjectInfo.Dataset, sDateParsed, eDateParsed, groupBy, orderBy)
 
 	return toReturn, nil
 }
